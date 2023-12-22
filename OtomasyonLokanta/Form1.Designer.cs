475237e727d004074cc1a7a4c2f9d69bb2433b22
murat ciplak <muratapk@hotmail.com>
2023-12-22 06:16:21 +0300

    Add project files.

---
+++ OtomasyonLokanta/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace OtomasyonLokanta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.masalarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personellerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rollerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kategorilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.servislerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siparislerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adisyonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.masalarToolStripMenuItem,
+            this.personellerToolStripMenuItem,
+            this.rollerToolStripMenuItem,
+            this.kategorilerToolStripMenuItem,
+            this.servislerToolStripMenuItem,
+            this.siparislerToolStripMenuItem,
+            this.adisyonToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1128, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // masalarToolStripMenuItem
+            // 
+            this.masalarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem});
+            this.masalarToolStripMenuItem.Name = "masalarToolStripMenuItem";
+            this.masalarToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.masalarToolStripMenuItem.Text = "Masalar";
+            // 
+            // personellerToolStripMenuItem
+            // 
+            this.personellerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem1});
+            this.personellerToolStripMenuItem.Name = "personellerToolStripMenuItem";
+            this.personellerToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.personellerToolStripMenuItem.Text = "Personeller";
+            // 
+            // rollerToolStripMenuItem
+            // 
+            this.rollerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem6});
+            this.rollerToolStripMenuItem.Name = "rollerToolStripMenuItem";
+            this.rollerToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.rollerToolStripMenuItem.Text = "Roller";
+            // 
+            // kategorilerToolStripMenuItem
+            // 
+            this.kategorilerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem2});
+            this.kategorilerToolStripMenuItem.Name = "kategorilerToolStripMenuItem";
+            this.kategorilerToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.kategorilerToolStripMenuItem.Text = "Kategoriler";
+            // 
+            // servislerToolStripMenuItem
+            // 
+            this.servislerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem5});
+            this.servislerToolStripMenuItem.Name = "servislerToolStripMenuItem";
+            this.servislerToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.servislerToolStripMenuItem.Text = "Servisler";
+            // 
+            // siparislerToolStripMenuItem
+            // 
+            this.siparislerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem3});
+            this.siparislerToolStripMenuItem.Name = "siparislerToolStripMenuItem";
+            this.siparislerToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.siparislerToolStripMenuItem.Text = "Siparisler";
+            // 
+            // adisyonToolStripMenuItem
+            // 
+            this.adisyonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem4});
+            this.adisyonToolStripMenuItem.Name = "adisyonToolStripMenuItem";
+            this.adisyonToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.adisyonToolStripMenuItem.Text = "Adisyon";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // ekleToolStripMenuItem
+            // 
+            this.ekleToolStripMenuItem.Name = "ekleToolStripMenuItem";
+            this.ekleToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem.Text = "Ekle";
+            // 
+            // ekleToolStripMenuItem1
+            // 
+            this.ekleToolStripMenuItem1.Name = "ekleToolStripMenuItem1";
+            this.ekleToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem1.Text = "Ekle";
+            // 
+            // ekleToolStripMenuItem2
+            // 
+            this.ekleToolStripMenuItem2.Name = "ekleToolStripMenuItem2";
+            this.ekleToolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem2.Text = "Ekle";
+            // 
+            // ekleToolStripMenuItem3
+            // 
+            this.ekleToolStripMenuItem3.Name = "ekleToolStripMenuItem3";
+            this.ekleToolStripMenuItem3.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem3.Text = "Ekle";
+            // 
+            // ekleToolStripMenuItem4
+            // 
+            this.ekleToolStripMenuItem4.Name = "ekleToolStripMenuItem4";
+            this.ekleToolStripMenuItem4.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem4.Text = "Ekle";
+            // 
+            // ekleToolStripMenuItem5
+            // 
+            this.ekleToolStripMenuItem5.Name = "ekleToolStripMenuItem5";
+            this.ekleToolStripMenuItem5.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem5.Text = "Ekle";
+            // 
+            // ekleToolStripMenuItem6
+            // 
+            this.ekleToolStripMenuItem6.Name = "ekleToolStripMenuItem6";
+            this.ekleToolStripMenuItem6.Size = new System.Drawing.Size(224, 26);
+            this.ekleToolStripMenuItem6.Text = "Ekle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 500);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Ana Menü";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem masalarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personellerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rollerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kategorilerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem servislerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem siparislerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem adisyonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem5;
+    }
+}
+
---
+++ OtomasyonLokanta/Masalar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtomasyonLokanta
+{
+    public partial class Masalar : Form
+    {
+        public Masalar()
+        {
+            InitializeComponent();
+        }
+        LokantaDbEntities baglan=new LokantaDbEntities();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Masalars masabos = new Masalars();
+                masabos.Masa_Ad = textBox1.Text;
+                masabos.Masa_Durum = checkBox1.Checked;
+                baglan.Masalars.Add(masabos);
+                baglan.SaveChanges();
+                MessageBox.Show("Kaydınız Yapılmıştır");
+                doldur();
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show(ex.Message); 
+            }
+
+           
+        }
+        void doldur()
+        {
+            var listem = baglan.Masalars.ToList();
+            dataGridView1.DataSource = listem;
+        }
+
+        private void Masalar_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int satir=dataGridView1.CurrentCell.RowIndex;
+            textBox2.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
+            textBox1.Text = dataGridView1.Rows[satir].Cells[1].Value.ToString();
+            checkBox1.Checked =Convert.ToBoolean( dataGridView1.Rows[satir].Cells[2].Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Id = Convert.ToInt16(textBox2.Text);
+                var bul = baglan.Masalars.Find(Id);
+                baglan.Masalars.Remove(bul);
+                baglan.SaveChanges();
+                MessageBox.Show("Kaydınız Silimiştir");
+                doldur();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Kaydınız Silinmiştir.");
+            }
+           
+        }
+    }
+}
